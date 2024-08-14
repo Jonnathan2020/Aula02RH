94cec81b685a007cb89c255ed9f0740ddd27cefb
Jonnathan2020 <jonnathan.santos15@gmail.com>
2024-08-14 19:57:56 -0300

    Mensagem de atualização

--- obj/Debug/net8.0/Aula02RH.AssemblyInfo.cs
+++ obj/Debug/net8.0/Aula02RH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02RH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2959e1248e11495b3ae5a63b171b017c51654c78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02RH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02RH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
